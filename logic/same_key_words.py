8040b40394445125138f903b6a4af759737a9d82
lizonn <savenkoliza1@gmail.com>
2023-07-05 02:56:17 +0300

    synonyms

--- logic/same_key_words.py
+++ logic/same_key_words.py
@@ -8,17 +8,17 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 
 
-model_path = 'data/similar_words_model.bin/model.bin'
-
-
-model = gensim.models.KeyedVectors.load_word2vec_format(model_path, binary=True)
-
-def find_most_similar(word: str, words_list: list):
-    most_similar = model.most_similar_to_given(word, words_list)
-    return most_similar
-
-words_list = ['сахар_NOUN', 'торт_NOUN', 'чипсы_NOUN', 'печенье_NOUN']
-
-# Найдите наиболее подходящее слово из списка слов
-most_similar_word = find_most_similar('картофель_NOUN', words_list)
-print(most_similar_word)
+# model_path = 'data/similar_words_model.bin/model.bin'
+#
+#
+# model = gensim.models.KeyedVectors.load_word2vec_format(model_path, binary=True)
+#
+# def find_most_similar(word: str, words_list: list):
+#     most_similar = model.most_similar_to_given(word, words_list)
+#     return most_similar
+#
+# words_list = ['сахар_NOUN', 'торт_NOUN', 'чипсы_NOUN', 'печенье_NOUN']
+#
+# # Найдите наиболее подходящее слово из списка слов
+# most_similar_word = find_most_similar('картофель_NOUN', words_list)
+# print(most_similar_word)
